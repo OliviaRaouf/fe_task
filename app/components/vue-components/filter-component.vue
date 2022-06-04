760aaf07ba0db0e3ff1c0ca352a93d1061245061
Olivia Raouf <oliviaraouf60@gmail.com>
2022-06-04 19:32:57 +0200

    Sales Team table filter in progress

---
+++ app/components/vue-components/filter-component.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="c-chart__container">
+    <button @click="test">Click ME </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "filter-component",
+
+
+
+  data() {
+    return {
+       poula: "Hiii"
+    }
+    },
+    methods:{
+      test(){
+          this.$emit("hii", this.poula)
+      }
+    }
+}
+
+</script>
--- app/pages/home-page/home-page.controller.js
+++ app/pages/home-page/home-page.controller.js
@@ -1,6 +1,14 @@
+import Vue from 'vue';
+import 'ngVue';
+import 'ngVue/build/plugins.js';
+import filterComponent from '../../components/vue-components/filter-component.vue';
+
 angular
   .module('appModule')
-  .controller('homeController', homePageController);
+  .controller('homeController', homePageController)
+  .directive('filterComponent', createVueComponent => {
+    return createVueComponent(Vue.component('filterComponent', filterComponent));
+  });
 
 function homePageController(Employees) {
   const homePageVm = this;
@@ -13,5 +21,12 @@ function homePageController(Employees) {
       .then(({ data }) => {
         homePageVm.employees = homePageVm.employees.concat(data.employees);
       });
+  }
+  handleHelloEvent();
+  function handleHelloEvent(greetings) {
+    // console.log($scope);
+    // this.handleHelloEvent = function (greetings) {
+      console.log(greetings); // "Hello, World!"
+    // };
   }
 }
